fcad74e1a0c66446a12f37d8d33c43b1b8042a9b
Mauren Walter <mauren.d'avila@ecomp.ufsm.br>
2023-06-12 18:55:33 -0300

    init files, add lib GDAL, add lib ONNX

---
+++ RoofSeg.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using OSGeo.GDAL;
+using OSGeo.OGR;
+using OSGeo.OSR;
+
+
+class RoofSeg
+{
+    static void Main()
+    {
+        
+    }
+}
